4c86bea33584477fa2013ac69dd7b7f889baf757
Stayhye <165840599+Stayhye@users.noreply.github.com>
2025-03-02 03:01:54 -0600

    Update init.c

--- src/drivers/ps2/init.c
+++ src/drivers/ps2/init.c
@@ -111,20 +111,13 @@ void InitPS2()
 
     sbv_patch_enable_lmb();
 
-    //SifExecModuleBuffer(&iomanX_irx, size_iomanX_irx, 0, NULL, NULL);
-    //SifExecModuleBuffer(&fileXio_irx, size_fileXio_irx, 0, NULL, NULL);
-    //SifExecModuleBuffer(&freesio2_irx, size_freesio2_irx, 0, NULL, NULL);
-
-    SifLoadModule("rom0:XSIO2MAN", 0, NULL);
-	  SifLoadModule("rom0:XMCMAN", 0, NULL);
-	  SifLoadModule("rom0:XMCSERV", 0, NULL);
-	  SifLoadModule("rom0:XPADMAN", 0, NULL);
-    SifLoadModule("rom0:XMTAPMAN", 0, NULL);
-
-    //SifExecModuleBuffer(&mcman_irx, size_mcman_irx, 0, NULL, NULL);
-    //SifExecModuleBuffer(&mcserv_irx, size_mcserv_irx, 0, NULL, NULL);
-    //SifExecModuleBuffer(&freemtap_irx, size_freemtap_irx, 0, NULL, NULL);
-    //SifExecModuleBuffer(&freepad_irx, size_freepad_irx, 0, NULL, NULL);
+    SifExecModuleBuffer(&iomanX_irx, size_iomanX_irx, 0, NULL, NULL);
+    SifExecModuleBuffer(&fileXio_irx, size_fileXio_irx, 0, NULL, NULL);
+    SifExecModuleBuffer(&freesio2_irx, size_freesio2_irx, 0, NULL, NULL);
+    SifExecModuleBuffer(&mcman_irx, size_mcman_irx, 0, NULL, NULL);
+    SifExecModuleBuffer(&mcserv_irx, size_mcserv_irx, 0, NULL, NULL);
+    SifExecModuleBuffer(&freemtap_irx, size_freemtap_irx, 0, NULL, NULL);
+    SifExecModuleBuffer(&freepad_irx, size_freepad_irx, 0, NULL, NULL);
 
 #ifdef SOUND_ON
     SifExecModuleBuffer(&freesd_irx, size_freesd_irx, 0, NULL, NULL);
